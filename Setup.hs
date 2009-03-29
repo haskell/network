47cdd637d8253b2a4add8f338da4941d889f9046
Johan Tibell <johan.tibell@gmail.com>
2009-03-29 22:50:20 +0200

    Renamed Setup.lhs to Setup.hs for consistency.

---
+++ Setup.hs
@@ -0,0 +1,10 @@
+module Main (main) where
+
+import Distribution.Simple (defaultMainWithHooks, runTests, simpleUserHooks)
+import System.Cmd (system)
+
+main :: IO ()
+main = defaultMainWithHooks $ simpleUserHooks { runTests = runTests' } where
+  runTests' _ _ _ _ = do
+    system "runhaskell -i./dist/build tests/Simple.hs"
+    return ()
