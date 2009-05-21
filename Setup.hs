e811f44f57df07ebdfdc4ad7a4378229de276322
Johan Tibell <johan.tibell@gmail.com>
2009-05-21 15:32:29 +0200

    Check if the library has been built before running tests.

--- Setup.hs
+++ Setup.hs
@@ -1,10 +1,17 @@
 module Main (main) where
 
+import Control.Monad (unless)
 import Distribution.Simple (defaultMainWithHooks, runTests, simpleUserHooks)
+import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(..))
+import Distribution.Simple.Utils (die)
 import System.Cmd (system)
+import System.Directory (doesDirectoryExist)
 
 main :: IO ()
-main = defaultMainWithHooks $ simpleUserHooks { runTests = runTests' } where
-  runTests' _ _ _ _ = do
-    system "runhaskell -i./dist/build tests/Simple.hs"
-    return ()
+main = defaultMainWithHooks $ simpleUserHooks { runTests = runTests' }
+    where
+      runTests' _ _ _ lbi = do
+          built <- doesDirectoryExist $ buildDir lbi
+          unless built $ die "Run the 'build' command first."
+          system "runhaskell -i./dist/build tests/Simple.hs"
+          return ()
