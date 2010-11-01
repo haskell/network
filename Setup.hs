8f8daa3967def06bb2c945aee7a90faab4b4bafd
Johan Tibell <johan.tibell@gmail.com>
2010-11-01 23:00:29 +0100

    Fixed build problem in Setup.hs

--- Setup.hs
+++ Setup.hs
@@ -1,17 +1,6 @@
 module Main (main) where
 
-import Control.Monad (unless)
-import Distribution.Simple (defaultMainWithHooks, runTests, simpleUserHooks)
-import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(..))
-import Distribution.Simple.Utils (die)
-import System.Cmd (system)
-import System.Directory (doesDirectoryExist)
+import Distribution.Simple
 
 main :: IO ()
-main = defaultMainWithHooks $ simpleUserHooks { runTests = runTests' }
-    where
-      runTests' _ _ _ lbi = do
-          built <- doesDirectoryExist $ buildDir lbi
-          unless built $ die "Run the 'build' command first."
-          system "runhaskell -i./dist/build tests/Simple.hs"
-          return ()
+main = defaultMainWithHooks defaultUserHooks
