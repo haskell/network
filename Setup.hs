932e99b83cdd3dc0129ac5d52d309d5e594bae5f
Krzysztof Skrzętnicki <gtener@gmail.com>
2014-08-02 00:16:25 +0200

    first shot at providing Windows build without MSYS

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,13 @@
 module Main (main) where
 
 import Distribution.Simple
+import Distribution.System
+import System.Environment
 
 main :: IO ()
-main = defaultMainWithHooks autoconfUserHooks
+main = do
+  msys <- lookupEnv "MSYSTEM"
+  writeFile "log.txt" (show $ ("MSYSTEM",msys))
+  case (msys,buildOS) of
+    (Nothing, Windows) -> defaultMain -- on Windows without MSYS use preconfigured headers and params
+    _ -> defaultMainWithHooks autoconfUserHooks
