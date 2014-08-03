63350fd9da100aa40996ffa159ac9f234f726150
Krzysztof Skrzętnicki <gtener@gmail.com>
2014-08-03 10:46:27 +0200

    remove debugging command from Setup.hs

--- Setup.hs
+++ Setup.hs
@@ -7,7 +7,6 @@ import System.Environment
 main :: IO ()
 main = do
   msys <- lookupEnv "MSYSTEM"
-  writeFile "log.txt" (show $ ("MSYSTEM",msys))
   case (msys,buildOS) of
     (Nothing, Windows) -> defaultMain -- on Windows without MSYS use preconfigured headers and params
     _ -> defaultMainWithHooks autoconfUserHooks
