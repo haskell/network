0136050812622c4980725ed0f77726b82c748f5c
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-05-12 14:58:12 +0300

    Use cabal-doctest

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -1,20 +1,25 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Main (doctests)
+-- Copyright   :  (C) 2012-14 Edward Kmett
+-- License     :  BSD-style (see the file LICENSE)
+-- Maintainer  :  Edward Kmett <ekmett@gmail.com>
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- This module provides doctests for a project based on the actual versions
+-- of the packages it was built with. It requires a corresponding Setup.lhs
+-- to be added to the project
+-----------------------------------------------------------------------------
+module Main where
+
+import Build_doctests (flags, pkgs, module_sources)
+import Data.Foldable (traverse_)
 import Test.DocTest
 
 main :: IO ()
-main = doctest [
-    "-i"
-  , "-idist/build"
-  , "-i."
-  , "-idist/build/autogen"
-  , "-Idist/build/autogen"
-  , "-Idist/build"
-  , "-Iinclude"
-  , "-optP-include"
-  , "-optPdist/build/autogen/cabal_macros.h"
-  , "-DCALLCONV=ccall"
-  , "-XCPP"
-  , "-XDeriveDataTypeable"
-  , "-package-db dist/package.conf.inplace"
-  , "-package network"
-  , "Network"
-  ]
+main = do
+    traverse_ putStrLn args
+    doctest args
+  where
+    args = flags ++ pkgs ++ module_sources
