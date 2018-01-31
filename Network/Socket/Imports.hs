6b56a9615ff22723976efb9c6c9517a85a0f6379
Kazu Yamamoto <kazu@iij.ad.jp>
2018-01-31 12:29:14 +0900

    style change.

---
+++ Network/Socket/Imports.hs
@@ -0,0 +1,27 @@
+module Network.Socket.Imports (
+    ByteString
+  , module Control.Applicative
+  , module Control.Monad
+  , module Data.Bits
+  , module Data.Int
+  , module Data.List
+  , module Data.Maybe
+  , module Data.Monoid
+  , module Data.Ord
+  , module Data.Typeable
+  , module Data.Word
+  , module Numeric
+  ) where
+
+import Control.Applicative
+import Control.Monad
+import Data.Bits
+import Data.ByteString (ByteString)
+import Data.Int
+import Data.List
+import Data.Maybe
+import Data.Monoid
+import Data.Ord
+import Data.Typeable
+import Data.Word
+import Numeric
