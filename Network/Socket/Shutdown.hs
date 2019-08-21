5ee9828783ffac4c43d92868ffe0bf15b61b0b43
Kazu Yamamoto <kazu@iij.ad.jp>
2019-08-22 05:37:19 +0900

    Using IO/Timer manager instead of busy loop.

--- Network/Socket/Shutdown.hs
+++ Network/Socket/Shutdown.hs
@@ -9,9 +9,11 @@ module Network.Socket.Shutdown (
   , gracefulClose
   ) where
 
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (putMVar, takeMVar, newEmptyMVar)
 import qualified Control.Exception as E
 import Foreign.Marshal.Alloc (mallocBytes, free)
+import qualified GHC.Event as Ev
+import System.Posix.Types (Fd(..))
 
 import Network.Socket.Buffer
 import Network.Socket.Imports
@@ -41,6 +43,7 @@ shutdown s stype = void $ withFdSocket s $ \fd ->
 foreign import CALLCONV unsafe "shutdown"
   c_shutdown :: CInt -> CInt -> IO CInt
 
+data Wait = MoreData | TimeoutTripped
 
 -- | Closing a socket gracefully.
 --   This sends TCP FIN and check if TCP FIN is received from the peer.
@@ -49,31 +52,42 @@ foreign import CALLCONV unsafe "shutdown"
 --
 --   Since: 3.1.1.0
 gracefulClose :: Socket -> Int -> IO ()
-gracefulClose s tm = (sendRecvFIN `E.finally` close s) `E.catch` ignore
+gracefulClose s tmout = (sendRecvFIN `E.finally` close s) `E.catch` ignore
   where
     sendRecvFIN = do
         -- Sending TCP FIN.
         shutdown s ShutdownSend
         -- Waiting TCP FIN.
         E.bracket (mallocBytes bufSize) free recvEOF
-    recvEOF buf = loop 0
-      where
-        loop delay = do
-            -- We don't check the (positive) length.
-            -- In normal case, it's 0. That is, only FIN is received.
-            -- In error cases, data is available. But there is no
-            -- application which can read it. So, let's stop receiving
-            -- to prevent attacks.
-            r <- recvBufNoWait s buf bufSize
-            let delay' = delay + clock
-            when (r == -1 && delay' < tm) $ do
-                threadDelay (clock * 1000)
-                loop delay'
+    recvEOF buf = do
+        Just evmgr <- Ev.getSystemEventManager
+        tmmgr <- Ev.getSystemTimerManager
+        mvar <- newEmptyMVar
+        E.bracket (setup evmgr tmmgr mvar) (teardown evmgr tmmgr) $ \_ -> do
+            wait <- takeMVar mvar
+            case wait of
+              TimeoutTripped -> return ()
+              -- We don't check the (positive) length.
+              -- In normal case, it's 0. That is, only FIN is received.
+              -- In error cases, data is available. But there is no
+              -- application which can read it. So, let's stop receiving
+              -- to prevent attacks.
+              MoreData       -> void $ recvBufNoWait s buf bufSize
+    setup evmgr tmmgr mvar = do
+        -- millisecond to microsecond
+        key1 <- Ev.registerTimeout tmmgr (tmout * 1000) $
+            putMVar mvar TimeoutTripped
+        key2 <- withFdSocket s $ \fd' -> do
+            let callback _ _ = putMVar mvar MoreData
+                fd = Fd fd'
+            Ev.registerFd evmgr callback fd Ev.evtRead Ev.OneShot
+        return (key1, key2)
+    teardown evmgr tmmgr (key1,key2) = do
+        Ev.unregisterTimeout tmmgr key1
+        Ev.unregisterFd evmgr key2
     -- Don't use 4092 here. The GHC runtime takes the global lock
     -- if the length is over 3276 bytes in 32bit or 3272 bytes in 64bit.
     bufSize = 1024
-    -- milliseconds. Taken from BSD fast clock value.
-    clock = 200
     -- shutdown sometime returns ENOTCONN.
     -- Probably, we don't want to log this error.
     ignore :: E.IOException -> IO ()
