05afe8568145842bccaf9ac561a1ae41ed1fc2a8
ComplYue <compl.yue@icloud.com>
2020-05-18 15:10:45 +0800

    use openSocket in echo examples

--- examples/EchoClient.hs
+++ examples/EchoClient.hs
@@ -23,8 +23,6 @@ runTCPClient host port client = withSocketsDo $ do
     resolve = do
         let hints = defaultHints { addrSocketType = Stream }
         head <$> getAddrInfo (Just hints) (Just host) (Just port)
-    open addr = E.bracketOnError
-        (socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr))
-        close $ \sock -> do
-            connect sock $ addrAddress addr
-            return sock
+    open addr = E.bracketOnError (openSocket addr) close $ \sock -> do
+        connect sock $ addrAddress addr
+        return sock
