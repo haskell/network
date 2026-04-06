836aba78891f879cb79177177767f7da959fb992
Tom McLaughlin <tom@codedown.io>
2026-04-06 10:35:09 -0700

    Don't try to implement waitAndCancelReadSocketSTM/waitAndCancelWriteSocketSTM on Windows

--- Network/Socket/STM.hs
+++ Network/Socket/STM.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE CPP #-}
-
 module Network.Socket.STM where
 
 import Control.Concurrent
@@ -14,11 +12,7 @@ waitReadSocketSTM s = fst <$> waitAndCancelReadSocketSTM s
 -- | STM action to wait until the socket is ready for reading and STM
 --   action to cancel the waiting.
 waitAndCancelReadSocketSTM :: Socket -> IO (STM (), IO ())
-#if !defined(mingw32_HOST_OS)
 waitAndCancelReadSocketSTM s = withFdSocket s $ threadWaitReadSTM . Fd
-#else
-waitAndCancelReadSocketSTM _ = undefined
-#endif
 
 -- | STM action to wait until the socket is ready for writing.
 waitWriteSocketSTM :: Socket -> IO (STM ())
@@ -27,8 +21,4 @@ waitWriteSocketSTM s = fst <$> waitAndCancelWriteSocketSTM s
 -- | STM action to wait until the socket is ready for writing and STM
 --   action to cancel the waiting.
 waitAndCancelWriteSocketSTM :: Socket -> IO (STM (), IO ())
-#if !defined(mingw32_HOST_OS)
 waitAndCancelWriteSocketSTM s = withFdSocket s $ threadWaitWriteSTM . Fd
-#else
-waitAndCancelWriteSocketSTM _ = undefined
-#endif
