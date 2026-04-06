8249616c646d0bee62f370728015eb5510115468
Tom McLaughlin <tom@codedown.io>
2026-04-06 10:19:01 -0700

    Fix socketToHandle

--- Network/Socket/Handle.hs
+++ Network/Socket/Handle.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE CPP #-}
+
 module Network.Socket.Handle where
 
 import qualified GHC.IO.Device (IODeviceType (Stream))
 import GHC.IO.Handle.FD (fdToHandle')
 import System.IO (BufferMode (..), Handle, IOMode (..), hSetBuffering)
+
+#if defined(mingw32_HOST_OS) && defined(HAS_WINIO)
+import Foreign.Ptr (wordPtrToPtr)
+import GHC.IO.SubSystem ((<!>))
+import qualified GHC.Event.Windows as Mgr
+import GHC.IO.Windows.Handle (fromHANDLE, Io, NativeHandle)
+import GHC.IO.Handle.Windows (mkHandleFromHANDLE)
+#endif
 
 import Network.Socket.Types
 
@@ -19,11 +29,20 @@ import Network.Socket.Types
 -- Haskell, e.g. merely performing 'hClose' on a TCP socket won't
 -- cooperate with peer's 'gracefulClose', i.e. proper shutdown
 -- sequence with appropriate handshakes specified by the protocol.
--- TODO: WinIO doesn't use fd, add support
---       Need to remove fromIntegral.
 socketToHandle :: Socket -> IOMode -> IO Handle
 socketToHandle s mode = invalidateSocket s err $ \oldfd -> do
     h <-
+#if defined(mingw32_HOST_OS) && defined(HAS_WINIO)
+        socketToHandleMIO oldfd <!> socketToHandleWinIO oldfd
+#else
+        socketToHandleMIO oldfd
+#endif
+    hSetBuffering h NoBuffering
+    return h
+  where
+    err _ = ioError $ userError $ "socketToHandle: socket is no longer valid"
+
+    socketToHandleMIO oldfd =
         fdToHandle'
             (fromIntegral oldfd)
             (Just GHC.IO.Device.Stream)
@@ -31,7 +50,11 @@ socketToHandle s mode = invalidateSocket s err $ \oldfd -> do
             (show s)
             mode
             True {-bin-}
-    hSetBuffering h NoBuffering
-    return h
-  where
-    err _ = ioError $ userError $ "socketToHandle: socket is no longer valid"
+
+#if defined(mingw32_HOST_OS) && defined(HAS_WINIO)
+    socketToHandleWinIO oldfd = do
+        let hwnd = wordPtrToPtr $ fromIntegral oldfd
+        Mgr.associateHandle' hwnd
+        let nativeHwnd = fromHANDLE hwnd :: Io NativeHandle
+        mkHandleFromHANDLE nativeHwnd GHC.IO.Device.Stream (show s) mode Nothing
+#endif
